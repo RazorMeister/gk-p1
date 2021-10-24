77fff24fd803c90e94bc914305a17e01f04830ba
Tymoteusz Bartnik <battnik90@gmail.com>
2021-10-24 17:06:44 +0200

    Added antyaliasing

--- DrawHelper.cs
+++ DrawHelper.cs
@@ -7,7 +7,112 @@ namespace Projekt1
     {
         public const int DISTANCE = 20;
 
+        private static int ipart(double x) { return (int)x; }
+
+        private static int round(double x) { return ipart(x + 0.5); }
+
+        private static double fpart(double x)
+        {
+            if (x < 0) return (1 - (x - Math.Floor(x)));
+            return (x - Math.Floor(x));
+        }
+
+        private static double rfpart(double x)
+        {
+            return 1 - fpart(x);
+        }
+
+        private static void plot(Bitmap bm, double x, double y, double c, Color colorParam)
+        {
+            int alpha = (int)(c * 255);
+            if (alpha > 255) alpha = 255;
+            if (alpha < 0) alpha = 0;
+            Color color = Color.FromArgb(alpha, colorParam);
+            SetPixel(bm, (int)x, (int)y, color);
+        }
+
         public static void DrawLine(Bitmap bm, Point p1, Point p2, Color? colorParam = null)
+        {
+            Color color = colorParam ?? Color.Black;
+
+            double x0 = p1.X;
+            double y0 = p1.Y;
+            
+            double x1 = p2.X;
+            double y1 = p2.Y;
+
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            double temp;
+            if (steep)
+            {
+                temp = x0; x0 = y0; y0 = temp;
+                temp = x1; x1 = y1; y1 = temp;
+            }
+            if (x0 > x1)
+            {
+                temp = x0; x0 = x1; x1 = temp;
+                temp = y0; y0 = y1; y1 = temp;
+            }
+
+            double dx = x1 - x0;
+            double dy = y1 - y0;
+            double gradient = dy / dx;
+
+            double xEnd = Math.Round(x0);
+            double yEnd = y0 + gradient * (xEnd - x0);
+            double xGap = rfpart(x0 + 0.5);
+            double xPixel1 = xEnd;
+            double yPixel1 = ipart(yEnd);
+
+            if (steep)
+            {
+                plot(bm, yPixel1, xPixel1, rfpart(yEnd) * xGap, color);
+                plot(bm, yPixel1 + 1, xPixel1, fpart(yEnd) * xGap, color);
+            }
+            else
+            {
+                plot(bm, xPixel1, yPixel1, rfpart(yEnd) * xGap, color);
+                plot(bm, xPixel1, yPixel1 + 1, fpart(yEnd) * xGap, color);
+            }
+            double intery = yEnd + gradient;
+
+            xEnd = round(x1);
+            yEnd = y1 + gradient * (xEnd - x1);
+            xGap = fpart(x1 + 0.5);
+            double xPixel2 = xEnd;
+            double yPixel2 = ipart(yEnd);
+            if (steep)
+            {
+                plot(bm, yPixel2, xPixel2, rfpart(yEnd) * xGap, color);
+                plot(bm, yPixel2 + 1, xPixel2, fpart(yEnd) * xGap, color);
+            }
+            else
+            {
+                plot(bm, xPixel2, yPixel2, rfpart(yEnd) * xGap, color);
+                plot(bm, xPixel2, yPixel2 + 1, fpart(yEnd) * xGap, color);
+            }
+
+            if (steep)
+            {
+                for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
+                {
+                    plot(bm, ipart(intery), x, rfpart(intery), color);
+                    plot(bm, ipart(intery) + 1, x, fpart(intery), color);
+                    intery += gradient;
+                }
+            }
+            else
+            {
+                for (int x = (int)(xPixel1 + 1); x <= xPixel2 - 1; x++)
+                {
+                    plot(bm, x, ipart(intery), rfpart(intery), color);
+                    plot(bm, x, ipart(intery) + 1, fpart(intery), color);
+                    intery += gradient;
+                }
+            }
+        }
+
+        public static void DrawLine1(Bitmap bm, Point p1, Point p2, Color? colorParam = null)
         {
             Color color = colorParam ?? Color.Black;
 
